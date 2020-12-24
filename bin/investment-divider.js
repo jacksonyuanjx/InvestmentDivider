b5b735100151d5c0cdcbc86ae595bcb0dbbcf441
jacksonyuanjx <jacksonyuanjx@hotmail.com>
2020-12-23 21:31:24 -0800

    refactored code into subfiles in /lib dir

--- bin/investment-divider.js
+++ bin/investment-divider.js
@@ -1,15 +1,10 @@
 #!/usr/bin/env node
 
-"use strict";
-
 const { program } = require("commander");
-const inquirer = require("inquirer");
-const { performance } = require("perf_hooks");
 const Table = require("cli-table3");
 const kleur = require("kleur");
-const { getAssetDistribution } = require("../lib/algo");
-const questions = require("./questions");
 const investmentDividerVersion = require("../package.json").version;
+const { prompt } = require("../lib");
 // console.log(investmentDividerVersion);
 
 program
@@ -21,59 +16,59 @@ program
         // console.log('environment:', env || 'no environment given');
     });
 
-const getTickers = async () => {
-    const tickers = {};
-    const _recurse = async () => {
-        let tickerSymAns = await inquirer.prompt(questions.tickerSymbolQues);
-        while (!tickerSymAns.ticker_symbol.trim()) tickerSymAns = await inquirer.prompt(questions.tickerSymbolQuesEmptyStr);
-        while (tickerSymAns.ticker_symbol in tickers) tickerSymAns = await inquirer.prompt(questions.tickerSymbolQuesDuplicate);
-        const { ticker_symbol: tickerAns } = tickerSymAns;
+// const getTickers = async () => {
+//     const tickers = {};
+//     const _recurse = async () => {
+//         let tickerSymAns = await inquirer.prompt(questions.tickerSymbolQues);
+//         while (!tickerSymAns.ticker_symbol.trim()) tickerSymAns = await inquirer.prompt(questions.tickerSymbolQuesEmptyStr);
+//         while (tickerSymAns.ticker_symbol in tickers) tickerSymAns = await inquirer.prompt(questions.tickerSymbolQuesDuplicate);
+//         const { ticker_symbol: tickerAns } = tickerSymAns;
 
-        if (tickerAns !== "q") {
-            let tickerPriceAns = { ticker_price: NaN };
-            while (isNaN(tickerPriceAns.ticker_price)) {
-                tickerPriceAns = await inquirer.prompt({
-                    type: "number",
-                    name: "ticker_price",
-                    message: `What's the current price of ${tickerAns}: `,
-                });
-            }
-            tickers[tickerAns] = tickerPriceAns.ticker_price;
-            await _recurse();
-        }
-    };
+//         if (tickerAns !== "q") {
+//             let tickerPriceAns = { ticker_price: NaN };
+//             while (isNaN(tickerPriceAns.ticker_price)) {
+//                 tickerPriceAns = await inquirer.prompt({
+//                     type: "number",
+//                     name: "ticker_price",
+//                     message: `What's the current price of ${tickerAns}: `,
+//                 });
+//             }
+//             tickers[tickerAns] = tickerPriceAns.ticker_price;
+//             await _recurse();
+//         }
+//     };
 
-    await _recurse();
-    return tickers;
-};
+//     await _recurse();
+//     return tickers;
+// };
 
-const getTotalCapital = async (question) => {
-    const { total_capital_to_divide: totalCapital } = await inquirer.prompt(question);
-    return (isNaN(totalCapital)) ? getTotalCapital(questions.totalCapitalRetry) : totalCapital;
-};
+// const getTotalCapital = async (question) => {
+//     const { total_capital_to_divide: totalCapital } = await inquirer.prompt(question);
+//     return (isNaN(totalCapital)) ? getTotalCapital(questions.totalCapitalRetry) : totalCapital;
+// };
 
-const buildResponse = (result) => {
-    const table = new Table({
-        head: ["Ticker", "Price ($)", "# of Shares to Buy", "Total Value ($)"],
-        style: { head: ["yellow"], border: ["blue"]}
-    });
-    result.forEach(e => table.push([e[0], e[1], e[2], e[1]*e[2]]));
-    return table.toString();
-};
+// const buildResponse = (result) => {
+//     const table = new Table({
+//         head: ["Ticker", "Price ($)", "# of Shares to Buy", "Total Value ($)"],
+//         style: { head: ["yellow"], border: ["blue"]}
+//     });
+//     result.forEach(e => table.push([e[0], e[1], e[2], e[1]*e[2]]));
+//     return table.toString();
+// };
 
-const prompt = async () => {
-    const totalCapital = await getTotalCapital(questions.totalCapitalQues);
-    const tickers = await getTickers();
-    if (!Object.keys(tickers).length) return console.log("You must input at least one ticker");
-    // console.log(tickers);
-    // TODO: output log message saying computing results?
-    const startTime = performance.now();
-    const { result, sum } = getAssetDistribution(totalCapital, tickers);
+// const prompt = async () => {
+//     const totalCapital = await getTotalCapital(questions.totalCapitalQues);
+//     const tickers = await getTickers();
+//     if (!Object.keys(tickers).length) return console.log("You must input at least one ticker");
+//     // console.log(tickers);
+//     // TODO: output log message saying computing results?
+//     const startTime = performance.now();
+//     const { result, sum } = getAssetDistribution(totalCapital, tickers);
 
-    console.log(`Operation took: ${performance.now() - startTime}ms`);
-    console.log(buildResponse(result));
-    console.log(`The total amount invested is $${sum} out of a total of $${totalCapital} available.`);
-};
+//     console.log(`Operation took: ${performance.now() - startTime}ms`);
+//     console.log(buildResponse(result));
+//     console.log(`The total amount invested is $${sum} out of a total of $${totalCapital} available.`);
+// };
 
 prompt();
 
