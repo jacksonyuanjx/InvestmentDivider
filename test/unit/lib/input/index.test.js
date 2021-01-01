e13104d6856af50a180d76e2b866ce25042e14e1
jacksonyuanjx <jacksonyuanjx@hotmail.com>
2020-12-31 23:09:55 -0800

    unit tests for /lib/input getTickers() function

--- test/unit/lib/input/index.test.js
+++ test/unit/lib/input/index.test.js
@@ -4,7 +4,89 @@ const rewire = require("rewire");
 
 const input = rewire("../../../../lib/input");
 const { getTickers, getTotalCapital } = input;
-const { totalCapitalQues, totalCapitalRetry } = require("../../../../lib/questions");
+const {
+    tickerSymbolQues,
+    tickerSymbolQuesEmptyStr,
+    tickerSymbolQuesDuplicate,
+    totalCapitalQues,
+    totalCapitalRetry,
+} = require("../../../../lib/questions");
+
+describe("The getTickers() function", () => {
+    const stubs = {};
+    const rewireRestores = [];
+
+    beforeEach(() => {
+        stubs.inquirer = { prompt: sinon.stub() };
+
+        rewireRestores.push(input.__set__("inquirer", stubs.inquirer));
+    });
+
+    afterEach(() => {
+        sinon.restore();
+        stubs.inquirer.prompt.reset();
+        rewireRestores.forEach((restore) => restore());
+    });
+
+    it("should call inquirer.prompt() with correct question for first call and correct ticker price question", async () => {
+        stubs.inquirer.prompt.onCall(0).resolves({ ticker_symbol: "GOOG" });
+        stubs.inquirer.prompt.onCall(1).resolves({ ticker_price: 102.43 });
+        stubs.inquirer.prompt.onCall(2).resolves({ ticker_symbol: "q" });
+        await getTickers();
+
+        sinon.assert.calledWith(stubs.inquirer.prompt.firstCall, tickerSymbolQues);
+        sinon.assert.calledWith(stubs.inquirer.prompt.secondCall, {
+            type: "number",
+            name: "ticker_price",
+            message: "What's the current price of GOOG: ",
+        });
+        sinon.assert.calledWith(stubs.inquirer.prompt.thirdCall, tickerSymbolQues);
+        sinon.assert.callCount(stubs.inquirer.prompt, 3);
+    });
+
+    it("should call inquirer.prompt() with tickerSymbolQuesEmptyStr if first ticker input is empty string", async () => {
+        stubs.inquirer.prompt.onCall(0).resolves({ ticker_symbol: "" });
+        stubs.inquirer.prompt.onCall(1).resolves({ ticker_symbol: "q" });
+        await getTickers();
+
+        sinon.assert.calledWith(stubs.inquirer.prompt.firstCall, tickerSymbolQues);
+        sinon.assert.calledWith(stubs.inquirer.prompt.secondCall, tickerSymbolQuesEmptyStr);
+    });
+
+    it("should call inquirer.prompt() once w/ tickerSymbolQuesDuplicate if attempting to add duplicate ticker once", async () => {
+        stubs.inquirer.prompt.onCall(0).resolves({ ticker_symbol: "MSFT" });
+        stubs.inquirer.prompt.onCall(1).resolves({ ticker_price: 123.45 });
+        stubs.inquirer.prompt.onCall(2).resolves({ ticker_symbol: "MSFT" });
+        stubs.inquirer.prompt.onCall(3).resolves({ ticker_symbol: "q" });
+        await getTickers();
+
+        sinon.assert.calledWith(stubs.inquirer.prompt.getCall(3), tickerSymbolQuesDuplicate);
+        sinon.assert.callCount(stubs.inquirer.prompt, 4);
+    });
+
+    it("should call inquirer.prompt() w/ tickerSymbolQuesEmptyStr after duplicate input is found and subsequent input is empty string", async () => {
+        stubs.inquirer.prompt.onCall(0).resolves({ ticker_symbol: "MSFT" });
+        stubs.inquirer.prompt.onCall(1).resolves({ ticker_price: 123.45 });
+        stubs.inquirer.prompt.onCall(2).resolves({ ticker_symbol: "MSFT" });
+        stubs.inquirer.prompt.onCall(3).resolves({ ticker_symbol: "" });
+        stubs.inquirer.prompt.onCall(4).resolves({ ticker_symbol: "q" });
+        await getTickers();
+
+        sinon.assert.calledWith(stubs.inquirer.prompt.getCall(4), tickerSymbolQuesEmptyStr);
+        sinon.assert.callCount(stubs.inquirer.prompt, 5);
+    });
+
+    it("should return correct tickers and their respective prices in object", async () => {
+        stubs.inquirer.prompt.onCall(0).resolves({ ticker_symbol: "MSFT" });
+        stubs.inquirer.prompt.onCall(1).resolves({ ticker_price: 123.45 });
+        stubs.inquirer.prompt.onCall(2).resolves({ ticker_symbol: "GOOG" });
+        stubs.inquirer.prompt.onCall(3).resolves({ ticker_price: 102.43 });
+        stubs.inquirer.prompt.onCall(4).resolves({ ticker_symbol: "q" });
+        const result = await getTickers();
+
+        expect(result).to.be.deep.equal({ MSFT: 123.45, GOOG: 102.43 });
+    });
+});
 
 describe("The getTotalCapital() function", () => {
     const stubs = {};
